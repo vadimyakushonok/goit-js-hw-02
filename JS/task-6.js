bd81f3796117a408e1f472ae696d70682f501f1a
Vadim <vyakushonok@gmail.com>
2021-01-16 21:32:09 +0300

    Обдумывание 6-й задачки

--- JS/task-6.js
+++ JS/task-6.js
@@ -2,3 +2,13 @@ let input;
 const numbers = [];
 let total = 0;
 
+do {
+    input = prompt('Введи число');
+    total += Number(input);
+} while(input !== null);
+
+alert(`Общая сумма чисел равна ${total}`);
+
+for (let number of numbers) {
+    sum += n
+}
